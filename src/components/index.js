635f1f2b0cc2e972c2cb77664282e252b4ff4f6d
Adam Patterson <hello@adampatterson.ca>
2020-11-26 18:39:09 -0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,3 @@ export { default as HitSearch } from './HitSearch';
 export { default as CustomHits } from './CustomHits';
 export { default as Banner } from './Banner';
 export { default as Slider } from './Slider';
-export { default as Tags } from './Tags';
